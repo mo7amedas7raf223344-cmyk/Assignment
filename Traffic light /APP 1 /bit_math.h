851f05a6c008f3f2a8dea8af19c81b483d5a4f72
Mohamed Ashraf <mo7amedas7raf223344@gmail.com>
2026-08-03 18:23:44 +0300

    Create bit_math.h

---
+++ Traffic light /APP 1 /bit_math.h
@@ -0,0 +1,4 @@
+#define SET_BIT(REG, BIT)    ((REG) |=  (1 << (BIT)))
+#define CLR_BIT(REG, BIT)    ((REG) &= ~(1 << (BIT)))
+#define TOG_BIT(REG, BIT)    ((REG) ^=  (1 << (BIT)))
+#define GET_BIT(REG, BIT)    (((REG) >> (BIT)) & 1)
